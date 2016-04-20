eff41e0e999fa83a14d3c2234b7cc068826aa440
Katherine Gonzalez <katherinegonzalez@Katherines-Mac-mini.local>
2016-04-19 20:59:05 -0600

    Initial Commit

---
+++ biblitecaOnline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  biblitecaOnline
+//
+//  Created by Katherine Gonzalez on 19/4/16.
+//  Copyright (c) 2016 Katherine Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
